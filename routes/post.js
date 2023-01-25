45d420d252866d81d81fedb4507cb1a5f4e3f042
flipott <ottphillipa@gmail.com>
2023-01-25 15:59:46 -0500

    Create basic GET and POST methods

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,106 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
+const { Schema } = require("mongoose");
 const router = require("express").Router();
+const mongoose = require('mongoose');
+
+// POSTS
 
 router.get("/", (req, res) => {
-    res.send("Post GET...")
-})
+    Post.find({}, (err, results) => {
+        if (err) {
+            return next(err);
+        }
+        res.send(results);
+    });
+});
+
+router.get("/:postId", (req, res, next) => {
+    Post.find({ id: req.params.postId }, (err, results) => {
+        if (err) {
+            return next(err);
+        }
+        res.send(results);
+    });
+});
+
+
+router.post("/", (req, res, next) => {
+    const post = new Post({
+        title: req.body.title,
+        body: req.body.body,
+        username: req.body.username,
+    }).save((err, results) => {
+        if (err) {
+            return next(err);
+        }
+        res.send(results);
+    });
+});
+
+// COMMENTS
+
+router.get("/:postId/:comments", (req, res, next) => {
+    Post.find({ id: req.params.postId }, (err, results) => {
+        if (err) {
+            return next(err);
+        }
+        res.send(results[0].comments);
+    });
+});
+
+router.post("/:postId/:comments", (req, res, next) => {
+    const comment = new Comment({
+        author: req.body.author,
+        body: req.body.body,
+    }).save((err, results) => {
+        if (err) {
+            return next(err);
+        }
+        Post.findByIdAndUpdate(
+            req.params.postId,
+            {$push: { comments: results }},
+            {safe: true, upsert: true, new : true},
+            (err, results) => {
+                if (err) {
+                    return next(err);
+                }
+                res.send("Successfully added comment to post.");
+            });
+    });
+});
+
+router.get("/:postId/:comments/:commentId", (req, res, next) => {
+    Comment.find({ id: req.params.commentId }, (err, results) => {
+        if (err) {
+            return next(err);
+        }
+        res.send(results[0]);
+    });
+});
+
+router.delete("/:postId/:comments/:commentId", (req, res, next) => {
+    Comment.findByIdAndDelete(req.params.commentId, (err, results) => {
+        if (err) {
+            return next(err);
+        }
+        Post.findByIdAndUpdate(
+            req.params.postId,
+            {$pull: { "comments._id" : mongoose.id(req.params.commentId)} },
+            {safe: true, new: true},
+            (err, results) => {
+                if (err) {
+                    return next(err);
+                }
+                res.send("Comment successfully deleted.");
+            });
+    });
+});
+
+
+
+
+
+
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,25 @@
 const User = require("../models/user");
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.send("User GET...")
-})
+router.get("/", (req, res, next) => {
+    User.find({}, (err, results) => {
+        if (err) {
+            return next(err);
+        }
+        res.send(results);
+    });
+});
+
+router.post("/", (req, res, next) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+    }).save(err => {
+        if (err) {
+            return next(err);
+        }
+        res.send(`New user created: ${req.body.username}`);
+    });
+});
 
 module.exports = router;
