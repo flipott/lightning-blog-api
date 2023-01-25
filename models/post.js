4eed81e146cb06c9ea13609d276eac0b4e121471
flipott <ottphillipa@gmail.com>
2023-01-25 13:30:36 -0500

    Create schemas and models

---
+++ models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    time: { type: Date, default: Date.now },
+    published: { type: Boolean, default: true },
+    username: { type: Schema.Types.ObjectId, required: true },
+    comments: { type: Array, default: []},
+});
+
+module.exports = mongoose.model("Post", PostSchema);
