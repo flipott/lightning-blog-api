70cc7540d8e64975490187a3060325454f110fac
flipott <ottphillipa@gmail.com>
2023-01-25 13:30:57 -0500

    Connect routes to app

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config()
+const routes = require("./routes");
 
+
+require('dotenv').config()
 const mongoDb = process.env.DB;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "Error connecting to Mongo DB"));
 console.log(db);
 const app = express();
+
+//Routes
+app.use('/user', routes.user);
+app.use('/post', routes.post);
+app.use('/comment', routes.comment);
 
 
 
